cfe3568f231bfb5e91cda7c2d3618baab3ec3405
supercomet329 <supercomet329@gmail.com>
2019-05-15 10:05:14 -0600

    Add docblock for EmailInterface

--- src/App/Models/EmailInterface.php
+++ src/App/Models/EmailInterface.php
@@ -6,5 +6,10 @@ use Zend\Stdlib\ArraySerializableInterface;
 
 interface EmailInterface extends ArraySerializableInterface
 {
+    /**
+     * Config representation that a Client can send
+     * @param string $apiKey
+     * @return array
+     */
     public function getConfig(string $apiKey): array;
 }
